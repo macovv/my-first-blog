8bc06411b2ad3951eb6f5e8c3a9d6c20b6636731
macovv <szymonwojtala122@gmail.com>
2017-07-14 12:12:01 +0200

    Modyfikacja szablonu, aby wyświtał posty

--- blogapp/views.py
+++ blogapp/views.py
@@ -2,7 +2,9 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+from .models import Post
 
 # Create your views here.
 def post_list(request):
-    return render(request, 'blogapp/post_list.html',{})
+    posts = Post.objects.all().order_by('title')
+    return render(request, 'blogapp/post_list.html',{'posts':posts})
