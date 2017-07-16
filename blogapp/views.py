be4adbf39c22eb2d36001e37c15dc92954e4cd60
macovv <szymonwojtala122@gmail.com>
2017-07-16 03:20:49 +0200

    Dodano style, oraz widok i szablon dla pojedycznego wpisu na stronie

--- blogapp/views.py
+++ blogapp/views.py
@@ -1,10 +1,12 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
-from django.shortcuts import render
 from .models import Post
-
+from django.shortcuts import render, get_object_or_404
 # Create your views here.
 def post_list(request):
     posts = Post.objects.all().order_by('title')
     return render(request, 'blogapp/post_list.html',{'posts':posts})
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, "blogapp/post_detail.html",{'post':post})
